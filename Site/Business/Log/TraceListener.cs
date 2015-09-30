e3b6df8be464b42f602e132201ec84df6b67fe93
unknown <PatrickKl@PC1565.macaw.nl>
2015-09-30 21:44:35 +0200

    Added trace listener

---
+++ Site/Business/Log/TraceListener.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Web;
+using EPiServer.Find.Tracing;
+using log4net;
+
+namespace Site.Business.Log
+{
+    public class TraceListener : ITraceListener
+    {
+        private ILog log = LogManager.GetLogger("EPiServer.Find");
+
+        public void Add(ITraceEvent traceEvent)
+        {
+            var message = new StringBuilder();
+            if (HttpContext.Current != null)
+            {
+                message.Append("During HTTP request to: ");
+                message.Append(HttpContext.Current.Request.Url);
+                message.AppendLine();
+            }
+
+            message.Append("Instance ");
+            message.Append(traceEvent.Source.TraceId);
+            message.Append(" of type ");
+            message.Append(traceEvent.Source.GetType());
+            message.Append(" reported: ");
+            message.Append(traceEvent.Message);
+            message.AppendLine();
+
+            if (traceEvent.IsError)
+            {
+                log.Error(message.ToString());
+            }
+            else
+            {
+                log.Debug(message.ToString());
+            }
+        }
+    }
+}
