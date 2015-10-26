9dab6a7c90c34a9a833ccea66f76d4f70b038111
unknown <PatrickKl@PC1565.macaw.nl>
2015-10-26 08:58:36 +0100

    no message

--- Lab/Program.cs
+++ Lab/Program.cs
@@ -29,43 +29,6 @@ namespace Lab
         }
 
         /// <summary>
-        /// Code solutons
-        /// </summary>
-        /// <param name="cli"></param>
-        static void Solutions(ITypeSearch<Hotel> q)
-        {
-            //Exercise 1: Filter on price -> Get all hotels that have price between 100 and 200
-            //return q.Filter(x => x.PriceUSD.InRange(100, 200));
-
-            //Exercise 2: Filter on rating/review -> Find all hotels that have a star rating of either 4 o 5, OR
-            //review rate of 9 or 10 with more than 50 reviews
-            /*return q.Filter(x => x.StarRating.InRange(4, 5))
-                    .OrFilter(x => x.ReviewCount.GreaterThan(50))
-                    .Filter(x => x.Ratings.Overall.InRange(8, 9));*/
-
-            //Exercise 3: Filter on location -> Get all hotels within 5 km of the cosmopolitan hotel,
-            //order by distance from cosmopolitan hotel (closest first)
-            /*return q.Filter(x => x.GeoCoordinates.WithinDistanceFrom(COSMOPOLITAN_HOTEL_GEO, 5.Kilometer()))
-                    .OrderBy(x => x.GeoCoordinates).DistanceFrom(COSMOPOLITAN_HOTEL_GEO);*/
-
-            //Exercise 4: Advanced filtering -> Show hotels with more than 2 stars within 10 km of the cosmopolitan hotel
-            //that offer room service, have air condition and are of chain specified by user
-            /*return q.Filter(x => x.StarRating.GreaterThan(2))
-                .Filter(x => x.GeoCoordinates.WithinDistanceFrom(COSMOPOLITAN_HOTEL_GEO, 10.Kilometer()))
-                .Filter(x => x.Features.MatchCaseInsensitive("Air conditioned"))
-                .Filter(x => x.Features.MatchCaseInsensitive("Room service"));*/
-
-            //Exercise 5: Range facets -> create range facets for price ranges 20-50, 51-100 and 101-150 USD
-            /*return q.RangeFacetFor(x => x.PriceUSD, new[] { new NumericRange(20, 50), new NumericRange(51, 100), new NumericRange(101, 150)});*/
-
-            //Exercise 6: Facet for country -> List name of all countries that have hotels
-            //return q.TermsFacetFor(x => x.Location.Country.Title, request => request.Size = 500).Take(0);
-
-            //Exercise 7: Basic text search, query entered by user, in fields Name and Description
-            /*return q.For("hotel").InFields(x => x.Name, x => x.Description);*/
-        }
-
-        /// <summary>
         /// Exercise 1: Filter on price -> Get all hotels that have price between 100 and 200
         /// </summary>
         /// <param name="q"></param>
